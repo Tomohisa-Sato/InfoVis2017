1ef3cd595dd997a16e83ea1b3e89686b27a85cde
佐藤　智久 <178x210x@istc-a-006.epc.center.kobe-u.ac.jp>
2017-04-12 14:06:01 +0900

    example1

---
+++ W02/vec3.js
@@ -0,0 +1,38 @@
+Vec3 = function(x,y,z)
+{
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+
+Vec3.prototype.add = function(v)
+{
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+    return this;
+}
+
+
+Vec3.prototype.sum = function()
+{
+    return this.x + this.y + this.z;
+}
+
+Vec3.prototype.min() = function()
+{
+    if((this.x <= this.y) && (this.x <= this.z))
+    {
+	return this.x;
+    }
+
+    if((this.y <= this.x) && (this.y <= this.z))
+    {
+	return this.y;
+    }
+
+    if((this.z <= this.x) && (this.z <= this.y))
+    {
+	return this.z;
+    }
+}
